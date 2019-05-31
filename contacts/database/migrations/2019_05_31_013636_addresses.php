7aefc9afa9c67661504241fa9e8e7fa5c5a5ca15
Rohan Mehta <rohan@phpwpinfo.com>
2019-05-31 07:24:38 +0530

    people and addresses table migration

---
+++ contacts/database/migrations/2019_05_31_013636_addresses.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Addresses extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('person_id');
+            $table->string('street_address');
+            $table->string('suburb');
+            $table->string('pincode');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
