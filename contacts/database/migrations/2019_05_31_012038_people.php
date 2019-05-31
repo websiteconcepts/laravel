7906e259ef122da3ee96dca9aa52236ee89c944b
Rohan Mehta <rohan@phpwpinfo.com>
2019-05-31 07:28:25 +0530

    migrated people table with phone numbers

--- contacts/database/migrations/2019_05_31_012038_people.php
+++ contacts/database/migrations/2019_05_31_012038_people.php
@@ -13,7 +13,19 @@ class People extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('people', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('fname');
+            $table->string('lname');
+            $table->date('dob')->nullable();
+            $table->string('company');
+            $table->string('position');
+            $table->string('email')->nullable();
+            $table->string('phone_mobile')->nullable();
+            $table->string('phone_work')->nullable();
+            $table->timestamps();
+        });
+        
     }
 
     /**
