94a467d69669b23f8eda59cafdc7c3d0459eb04b
Rohan Mehta <rohan@phpwpinfo.com>
2019-05-31 12:07:39 +0530

    implemented Basic Auth

--- contacts/resources/views/contacts/index.blade.php
+++ contacts/resources/views/contacts/index.blade.php
@@ -1,39 +1,60 @@
-@extends('base')
+@extends('layouts.app')
 
-@section('main')
-<div class="row">
-<div class="col-sm-12">
-    <h1 class="display-3">Contacts</h1>    
-  <table class="table table-striped">
-    <thead>
-        <tr>
-          <td>ID</td>
-          <td>Name</td>
-          <td>Email</td>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+    @if(Auth::check())
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">All Contacts</div>
 
-          <td colspan = 2>Actions</td>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($contacts as $contact)
-        <tr>
-            <td>{{$contact->id}}</td>
-            <td>{{$contact->first_name}} {{$contact->last_name}}</td>
-            <td>{{$contact->email}}</td>
-            <td>
-                <a href="{{ route('contacts.edit',$contact->id)}}" class="btn btn-primary">Edit</a>
-            </td>
-            <td>
-                <form action="{{ route('contacts.destroy', $contact->id)}}" method="post">
-                  @csrf
-                  @method('DELETE')
-                  <button class="btn btn-danger" type="submit">Delete</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-  </table>
-<div>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <table class="table table-striped">
+                    <thead>
+                        <tr>
+                        <td>ID</td>
+                        <td>Name</td>
+                        <td>Email</td>
+
+                        <td colspan = 2>Actions</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($contacts as $contact)
+                        <tr>
+                            <td>{{$contact->id}}</td>
+                            <td>{{$contact->first_name}} {{$contact->last_name}}</td>
+                            <td>{{$contact->email}}</td>
+                            <td>
+                                <a href="{{ route('contacts.edit',$contact->id)}}" class="btn btn-primary">Edit</a>
+                            </td>
+                            <td>
+                                <a href="{{ route('contacts.show',$contact->id)}}" class="btn btn-primary">View</a>
+                            </td>
+                            <td>
+                                <form action="{{ route('contacts.destroy', $contact->id)}}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger" type="submit">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                </div>
+            </div>
+        </div>
+    @endif
+    @if(Auth::guest())
+              <a href="/login" class="btn btn-info"> Login >></a>
+            @endif
+    </div>
 </div>
 @endsection
