618691741441f00a31da87c61970a021e0c80a0c
Rohan Mehta <rohan@phpwpinfo.com>
2019-05-31 06:54:19 +0530

    Migration of Users and Seeding 10 Users

---
+++ contacts/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call('UserTableSeeder');
+
+    }
+}
+
+class UserTableSeeder extends Seeder {
+
+    public function run()
+    {
+        factory(App\User::class, 10)->create()->each(function ($p) {
+            $p->save();
+        });
+    }
+
+}
